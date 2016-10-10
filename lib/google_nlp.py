1d055b6dfd497b8c32f7b05133d6755e82db7dcf
Sven Mkw <sven.mkw@gmail.com>
2016-10-10 10:23:25 +0100

    added new caches and finished summary prototype according to excel sheet

--- lib/google_nlp.py
+++ lib/google_nlp.py
@@ -5,10 +5,7 @@ from oauth2client.client import GoogleCredentials
 DISCOVERY_URL = ('https://{api}.googleapis.com/'
                  '$discovery/rest?version={apiVersion}')
 
-
-def google_sentiment_analysis(text):
-    '''Run a sentiment analysis request on text'''
-
+def google_service():
     http = httplib2.Http()
 
     credentials = GoogleCredentials.get_application_default().create_scoped(
@@ -16,18 +13,40 @@ def google_sentiment_analysis(text):
     http = httplib2.Http()
     credentials.authorize(http)
 
-    service = discovery.build('language', 'v1beta1', http=http,
+    return discovery.build('language', 'v1beta1', http=http,
                               discoveryServiceUrl=DISCOVERY_URL)
-
-    service_request = service.documents().analyzeSentiment(
-      body={
+    
+def google_sentiment_analysis(text):
+    '''Run a sentiment analysis request on text'''
+    body = {
         'document': {
-           'type': 'PLAIN_TEXT',
-           'content': text,
+            'type': 'PLAIN_TEXT',
+            'content': text
         }
-      })
+    }
+
+    service = google_service()
+    service_request = service.documents().analyzeSentiment(body=body)
 
     response = service_request.execute()
+    
     polarity = response['documentSentiment']['polarity']
     magnitude = response['documentSentiment']['magnitude']
     return (polarity, magnitude)
+
+def google_syntax_analysis(text):
+    '''Run a syntax analysis request on text''' 
+    body = {
+        'document': {
+            'type': 'PLAIN_TEXT',
+            'content': text,
+        },
+        'features': {
+            'extract_syntax': True,
+        }
+    }
+
+    service = google_service()
+    service_request = service.documents().annotateText(body=body)
+    response = service_request.execute()
+    return response
--- lib/tokenize_to_sentences.py
+++ lib/tokenize_to_sentences.py
@@ -2,6 +2,10 @@
 import nltk.data
 sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
+def trim_new_line(sentence):
+    return sentence.replace('\n', ' ')
+
 def tokenize_to_sentences(text):
-    return sentence_detector.tokenize(text.strip())
+    sentences = sentence_detector.tokenize(text.strip())
+    return [trim_new_line(s) for s in sentences]
     
